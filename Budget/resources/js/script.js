4ee6c047f347ddff37e4188978002e1cbb67ecfc
BiDD <luonghung.qb@gmail.com>
2018-10-26 00:05:09 +0700

    update budget at T6 0:05 20/10/2018

--- Budget/resources/js/script.js
+++ Budget/resources/js/script.js
@@ -1,48 +1,40 @@
-
-var DOMstrings = {
-    finalIncome : '.final-income',
-    incomeDisplay : '.incomeDisplay',
-    expenseDisplay : '.expenseDisplay',
-    optionChoice : "option-choice",
-    numberInput : 'number-input',
-    keyInput : 'key-input',
-    incomeList : '.income-list',
-    expenseList : '.expense-list'
-};
-
-
-
-/* Display -------------------------
-
-
-/* Calculate -----------------------
-
-/* Add item to list
-
-*/
-
-function addItem(){
+var budgetController = (function(){
     
-    var e = document.getElementById(DOMstrings.optionChoice);
-    var chosen = e.options[e.selectedIndex].text;
+    var x = 23;
     
-    var keyInput = document.getElementById(DOMstrings.keyInput).value;
-    var numberInput = document.getElementById(DOMstrings.numberInput).value;
-    var element;
-    var html = 's';
-    
-    if (chosen === "income"){
-        html = '<li><div class="col span-1-of-2"><span class="key">%key%</span></div><div class="col span-1-of-2 value"><span class="value">%value%</span><button onclick="removeItem()"><i class="icon ion-ios-close-circle-outline"></i></button></div></li>'
-        element = DOMstrings.incomeList;
+    var add = function(a){
+        return x+a;
     }
     
-    else if (chosen === "expense"){
-        html = '<li><div class="col span-1-of-2"><span class="key">%key%</span></div><div class="col span-1-of-2 value"><span class="value">%value%</span><button onclick="removeItem()"><i class="icon ion-ios-close-circle-outline"></i></button></div></li>'
-        element = DOMstrings.expenseList;
+    var minus = function(a){
+        return x-a;
     }
     
-    var newHtml = html.replace("%key%", keyInput);
-    newHtml = newHtml.replace("%value%", numberInput);
+    return {
+        publicText: function(b){
+            return add(b);
+        }
+        
+       
+    }
     
-    document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
-}
+})();
+
+var UIController = (function() {
+    
+    // Some code later
+    
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+    
+    var z = budgetCtrl.publicText(2);
+    
+    return {
+        anotherPublic: function(){
+            console.log(z);
+        }
+    }
+    
+    
+})(budgetController, UIController);
